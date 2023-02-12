eb78f3f6ff61a091dd8806a9dd560167b0c65139
Alla Ponurko <aponurko@gmail.com>
2023-02-12 02:05:40 +0100

    1112

--- Node/phpnew/functions.php
+++ Node/phpnew/functions.php
@@ -15,3 +15,40 @@ function greeting($name,$age,$color1,$color2)
     echo "Hello, my name is "."<span style='color:".$color1.";'>"."$name"."<br/>";
     echo "I am "."<span style={'color:".$color2.";'}>"."$age"."<br/>";
 }
+function comparison($n1,$n2)
+{
+    if($n1%$n2==0)
+        echo "$n1 "."кратно"." $n2"."<br/>";
+    else echo "условие не соблюдено"."<br/>";
+}
+function squareIsMore($n1,$n2)
+{
+    if($n1>$n2) {
+        $sq = $n1 * $n1;
+        echo "$sq"."<br/>";
+    }
+    else
+    {$sq=$n2*$n2;
+        echo "$sq"."<br/>";
+    }
+}
+function countOfDays($n)
+{
+    switch ($n) {
+        case "1":echo "Days in the month 31" . "<br/>";break;
+        case "2":echo "Days in the month 28" . "<br/>";break;
+        case "3":echo "Days in the month 31" . "<br/>";break;
+        case "4":echo "Days in the month 30" . "<br/>";break;
+        case "5":echo "Days in the month 31" . "<br/>";break;
+        case "6":echo "Days in the month 30" . "<br/>";break;
+        case "7":echo "Days in the month 31" . "<br/>";break;
+        case "8":echo "Days in the month 31" . "<br/>";break;
+        case "9":echo "Days in the month 30" . "<br/>";break;
+        case "10":echo"Days in the month 31" . "<br/>";break;
+        case "11":echo "Days in the month 30" . "<br/>";break;
+        case "12":echo "Days in the month 31" . "<br/>";break;
+        default: "Data is not correct" . "<br/>";
+    };
+
+
+}
---
+++ Node/phpnew/task2.php
@@ -0,0 +1,10 @@
+<?php
+require_once ('functions.php');
+echo "Проверить кратность двух чисел:"."<br/>";
+comparison(116,4);
+echo "Вывести квадрат большего из двух чисел:"."<br/>";
+squareIsMore(14,18);
+$month=2;
+echo "Вывести количество дней в месяце: Месяц="."$month."."<br/>";
+countOfDays($month);
+
